ec5454230c95f91d59e57f1c5779abdd53c088a2
Saeed Hassani <saeedh.code@gmail.com>
2024-05-24 14:28:00 +0330

    ✨ Feat: add number, boolean, null and undefined to search type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 export const magicObjectSearch = <T extends { [key: string | number]: any }>(
-	str: string,
+	searchValue: string | number | boolean | null | undefined,
 	options: T[],
 	properties: (keyof T)[],
 ): T[] => {
+	const searchValueStr = String(searchValue);
 	return options.filter(option => {
 		return properties.some(prop => {
 			const propValue = option[prop];
-			if (propValue === undefined || propValue === null) return false;
-			return propValue.toString().includes(str);
+			if (
+				typeof propValue === "string" ||
+				typeof propValue === "number"
+			) {
+				return String(propValue).includes(searchValueStr);
+			} else return propValue === searchValue;
 		});
 	});
 };
